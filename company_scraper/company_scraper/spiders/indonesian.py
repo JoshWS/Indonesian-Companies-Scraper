68b1235720c2510e9fb97fe2025c5683c132a605
Joshua Westphal da Silva <joshuasilva333@gmail.com>
2023-03-15 10:26:42 +0700

    Added working search terms.

--- company_scraper/company_scraper/spiders/indonesian.py
+++ company_scraper/company_scraper/spiders/indonesian.py
@@ -14,14 +14,21 @@ class IndonesianSpider(Spider):
     allowed_domains = ["companieshouse.id"]
 
     def start_requests(self):
-        for page in range(1, 135502):
-            url = f"https://companieshouse.id/?term=&page={page}"
+        searches = ["bui"]
+        for search in searches:
+            url = f"https://companieshouse.id/?term={search}"
             yield Request(
                 url,
                 callback=self.parse,
             )
 
     def parse(self, response):
+        yield self.parse_pages(response)
+        next_page = response.xpath("//a[@rel='next']/@href").extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_pages(self, response):
         l = ItemLoader(item=CompanyScraperItem(), response=response)
         names = l.get_xpath(
             "//ul[@class='py-2 text-sm']/li/div/a[1]/@title",
