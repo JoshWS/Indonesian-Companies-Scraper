4788a7b79b45258065d552b5ff4dd0336741e3d1
Joshua Westphal da Silva <joshuasilva333@gmail.com>
2023-03-17 13:53:01 +0700

    Added method toggle.

--- company_scraper/company_scraper/spiders/indonesian.py
+++ company_scraper/company_scraper/spiders/indonesian.py
@@ -9,43 +9,46 @@ import string
 # from news_scrapers.helpers import clean_html, re_replace, remove_tags
 from company_scraper.items import CompanyScraperItem
 
+GENERATE_TERMS = False
+
 
 class IndonesianSpider(Spider):
     name = "indonesian"
     allowed_domains = ["companieshouse.id"]
 
     def start_requests(self):
-        # with open("company_scraper/search_terms.txt") as f:
-        #     terms = f.readlines()
-        # for term in terms:
-        #     term = term.replace("\n", "")
-        #     url = f"https://companieshouse.id/?term={term}"
-        #     yield Request(
-        #         url, callback=self.parse, errback=self.errback, dont_filter=True
-        #     )
-
-        alpha = list(string.ascii_lowercase)
-        alpha.reverse()
-        alpha_stop = alpha[:2]
-        alpha_all = alpha
-        with open("company_scraper/search_terms.txt") as f:
-            terms = f.readlines()
-            with open("company_scraper/skip_terms.txt") as g:
-                skip = g.readlines()
-                print(skip)
-            for letter1 in alpha_stop:
-                for letter2 in alpha_stop:
-                    for letter3 in alpha_stop:
-                        term = f"{letter1}{letter2}{letter3}"
-                        if f"{term}\n" not in terms:
-                            if f"{term}\n" not in skip:
-                                url = f"https://companieshouse.id/?term={term}"
-                                yield Request(
-                                    url,
-                                    callback=self.parse,
-                                    errback=self.errback,
-                                    dont_filter=True,
-                                )
+        if GENERATE_TERMS == False:
+            with open("company_scraper/search_terms.txt") as f:
+                terms = f.readlines()
+            for term in terms:
+                term = term.replace("\n", "")
+                url = f"https://companieshouse.id/?term={term}"
+                yield Request(
+                    url, callback=self.parse, errback=self.errback, dont_filter=True
+                )
+        else:
+            alpha = list(string.ascii_lowercase)
+            alpha.reverse()
+            alpha_stop = alpha[:1]
+            alpha_all = alpha
+            with open("company_scraper/search_terms.txt") as f:
+                terms = f.readlines()
+                with open("company_scraper/skip_terms.txt") as g:
+                    skip = g.readlines()
+                    print(skip)
+                for letter1 in alpha_stop:
+                    for letter2 in alpha_stop:
+                        for letter3 in alpha_stop:
+                            term = f"{letter1}{letter2}{letter3}"
+                            if f"{term}\n" not in terms:
+                                if f"{term}\n" not in skip:
+                                    url = f"https://companieshouse.id/?term={term}"
+                                    yield Request(
+                                        url,
+                                        callback=self.parse,
+                                        errback=self.errback,
+                                        dont_filter=True,
+                                    )
 
     def parse(self, response):
         yield self.parse_pages(response)
